8a0739a343d1e57b9eb67228fc1be5d590ad58eb
keithmurphy09 <m4projectsanddesigns@gmail.com>
2026-02-10 16:36:48 +1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -58,13 +58,13 @@ async function renderApp() {
                         ${isAdmin ? '<span class="bg-red-600 px-2 py-1 rounded text-xs font-bold">ADMIN</span>' : ''}
                         
                         ${isAdmin ? `
-                            <button onclick="enterDemoMode('sole_trader');" class="no-glow px-3 py-2 md:px-4 md:py-2 bg-white hover:shadow-[0_0_10px_rgba(20,184,166,0.6)] rounded-lg text-sm border border-teal-400 text-gray-700">
+                            <button onclick="enterDemoMode('sole_trader');" class="no-glow px-3 py-2 md:px-4 md:py-2 bg-white hover:shadow-[0_0_10px_rgba(20,184,166,0.6)] rounded-lg text-xs border border-teal-400 text-gray-700">
                                 Sole Trader
                             </button>
-                            <button onclick="enterDemoMode('business');" class="no-glow px-3 py-2 md:px-4 md:py-2 bg-white hover:shadow-[0_0_10px_rgba(20,184,166,0.6)] rounded-lg text-sm border border-teal-400 text-gray-700">
+                            <button onclick="enterDemoMode('business');" class="no-glow px-3 py-2 md:px-4 md:py-2 bg-white hover:shadow-[0_0_10px_rgba(20,184,166,0.6)] rounded-lg text-xs border border-teal-400 text-gray-700">
                                 Business
                             </button>
-                            <button onclick="exitDemoMode();" class="no-glow px-3 py-2 md:px-4 md:py-2 bg-white hover:shadow-[0_0_10px_rgba(20,184,166,0.6)] rounded-lg text-sm border border-teal-400 text-gray-700">
+                            <button onclick="exitDemoMode();" class="no-glow px-3 py-2 md:px-4 md:py-2 bg-white hover:shadow-[0_0_10px_rgba(20,184,166,0.6)] rounded-lg text-xs border border-teal-400 text-gray-700">
                                 Real
                             </button>
                         ` : ''}
