172cdbf064557e9bf93bbc0fad18dfdc19dc5b90
keithmurphy09 <m4projectsanddesigns@gmail.com>
2026-03-16 19:58:33 +1000

    Update dashboard-enhance.js

--- js/dashboard-enhance.js
+++ js/dashboard-enhance.js
@@ -330,7 +330,7 @@ _dashObs.observe(document.body, { childList: true, subtree: true });
 console.log('Dashboard enhancements loaded');
 
 // Override user guide to open in browser tab instead of downloading
-window.downloadUserGuide = function(){ window.open('https://www.m4streamline.com/docs/Streamline_User_Guide.pdf', '_blank'); };
+window.downloadUserGuide = function(){ window.open('https://docs.google.com/viewer?url=https://www.m4streamline.com/docs/Streamline_User_Guide.pdf&embedded=true', '_blank'); };
 
 } catch(e) {
   console.error('Dashboard enhance error:', e);
